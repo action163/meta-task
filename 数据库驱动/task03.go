7cd4e891ef275fc55441815076509d84becfa0de
action163 <543262853@qq.com>
2025-06-25 13:51:55 +0800

    Create task03.go
    Sqlx 入门 - 使用Sql扩展库进行查询

---
+++ 数据库驱动/task03.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type Employee struct {
+	ID         int     `db:"id"`
+	Name       string  `db:"name"`
+	Department string  `db:"department"`
+	Salary     float64 `db:"salary"`
+}
+
+func main() {
+	dbConnStr := "root:p@ssw0rd@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := sqlx.Connect("mysql", dbConnStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	employees := []Employee{}
+	db.Select(&employees, "Select * from employees where department =?", "技术部")
+
+	for _, emp := range employees {
+		fmt.Println(emp.ID, emp.Name, emp.Department, emp.Salary)
+	}
+}
